7c5d7510751c4dd579694e9e76633d05a1ff0d3c
Hesham Said <heshamsaid@hotmail.com>
2024-04-26 11:57:45 +0300

    add run scripts

---
+++ scripts/run_all_index_comparison.sh
@@ -0,0 +1,13 @@
+#!bash
+
+EXPERIMENT='index_comparison'
+OUTPUT_FILE='index-comparison.csv'
+
+if ! test -f ${OUTPUT_FILE}; then
+    echo "benchmark_name,index_name,avg_query_time,avg_query_matches" > ${OUTPUT_FILE}
+fi
+
+./build/experiments/index_comparison_skewed_data_skewed_query
+./build/experiments/index_comparison_skewed_data_uniform_query
+./build/experiments/index_comparison_uniform_data_skewed_query
+./build/experiments/index_comparison_uniform_data_uniform_query
